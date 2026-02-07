237fdca69317b8edaabb5a874c421f18341438b6
Maura B <mmb228@pitt.edu>
2026-02-07 12:23:54 -0500

    work on index and modules and creating some new screens

--- FrontEnd_WebDev/script.js
+++ FrontEnd_WebDev/script.js
@@ -2,6 +2,14 @@
 // /GetLessons
 // /GetOverAllStatus
 
-function goToNewPage() {
+function goToModules() {
   window.location.href = "modules.html"; // Replace with your target URL
+}
+
+function goToFinance() {
+  window.location.href = "finance_module.html"; // Replace with your target URL
+}
+
+function goToSavingInvesting() {
+  window.location.href = "saving_investing.html"; // Replace with your target URL
 }
